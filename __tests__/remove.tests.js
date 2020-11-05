ad37daf5854e5d930fd2536b75146402251e0836
Cyprien Martine <cyp.martine@gmail.com>
2020-11-05 15:30:32 +0100

    Ajout des tests pour remove

---
+++ __tests__/remove.tests.js
@@ -0,0 +1,36 @@
+import request from 'supertest'
+import app from '../app'
+
+import * as clinicDependency from '../weiClinic'
+
+
+
+describe('Remove action', () => {
+    it('When the stack cannot be found', (done) => {
+        request(app)
+            .post('/remove/42')
+            .expect(400)
+            .end(done)
+    })
+    it('When the envelope cannot be found', (done) => {
+
+        clinicDependency.getClinic().create('male', 75, 'Bob')
+        clinicDependency.getClinic().envelopes[0].idStack = null
+
+        request(app)
+            .post('/remove/1')
+            .expect(400)
+            .end(done)
+    })
+    it('When we can remove', (done) => {
+        
+        clinicDependency.getClinic().create('male', 75, 'Bob')
+      
+        
+        request(app)
+            .post('/remove/2')
+            .expect(204)
+            .end(done)
+    })
+
+})
--- app.js
+++ app.js
@@ -24,18 +24,16 @@ app.get('/digitize', (req, res) => {
 
 
 app.post('/remove/:stackId', (request, response) => {
-    response.set({
-        'Content-Type': 'application/json'
-    })
+    
     let findStack = false
-    for (let i; i < getClinic.stacks.length; i++) {
-        if (getClinic.stacks[i].id === stackId) {
-            getClinic.stacks[i].idEnvelope = null
-            for (let j; j < getClinic.envelopes.legnth; j++) {
-                if (getClinic.envelopes[j].idStack === getClinic.stacks[i].id) {
-                    getClinic.envelopes[j].idStack = null
-		    findStack = true
-                }
+    for (let i = 0; i < getClinic().stacks.length; i++) {
+        if (getClinic().stacks[i].id == request.params.stackId) {
+            for (let j = 0; j < getClinic().envelopes.length; j++) {
+                if (getClinic().envelopes[j].idStack === getClinic().stacks[i].id) {
+                    getClinic().envelopes[j].idStack = null
+                    getClinic().stacks[i].idEnvelope = null
+		            findStack = true
+                } 
             }            
         }
     }
