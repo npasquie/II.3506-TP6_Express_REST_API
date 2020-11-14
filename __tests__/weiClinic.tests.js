c0b1d760f842d6b945d2e6653dbd31a90512302f
Cyprien Martine <cyp.martine@gmail.com>
2020-11-14 11:02:29 +0100

    Test de assignStackToEnvelope

--- __tests__/weiClinic.tests.js
+++ __tests__/weiClinic.tests.js
@@ -1,8 +1,88 @@
 import * as clinicDependency from '../src/weiClinic'
 
 
-describe('destroyStack', () => {
+describe('assignStackToEnvelope', () => {
 
+    it('can assign stack to an envelope', () => {
+        clinicDependency.getClinic().stacks = [{
+            id: 1,
+            realGender: 'M',
+            name: 'Bob',
+            age: 23,
+            idEnvelope: null
+        }]
+        clinicDependency.getClinic().envelopes = [{
+            id: 1,
+            gender: 'M',
+            age: 23,
+            idStack: null
+        }]
+
+        const response = clinicDependency.getClinic().assignStackToEnvelope(1, 1)
+        const status204 = {status: 204, message: `Stack 1 assigned to envelope 1`}
+        expect(response).toEqual(status204)
+    })
+
+    it('the stack is not found', () => {
+        clinicDependency.getClinic().stacks = []
+        clinicDependency.getClinic().envelopes = [{
+            id: 1,
+            gender: 'M',
+            age: 23,
+            idStack: null
+        }]
+
+        const response = clinicDependency.getClinic().assignStackToEnvelope(1, 1)
+        const error400 = {status: 400, message: `Stack 1 or envelope 1 not found`}
+        expect(response).toEqual(error400)
+    })
+
+    it('the envelope is not found', () => {
+        clinicDependency.getClinic().stacks = [{
+            id: 1,
+            realGender: 'M',
+            name: 'Bob',
+            age: 23,
+            idEnvelope: null
+        }]
+        clinicDependency.getClinic().envelopes = []
+
+        const response = clinicDependency.getClinic().assignStackToEnvelope(1, 1)
+        const error400 = {status: 400, message: `Stack 1 or envelope 1 not found`}
+        expect(response).toEqual(error400)
+    })
+
+    it('there are no backup envelope', () => {
+        clinicDependency.getClinic().stacks = [{
+            id: 1,
+            realGender: 'M',
+            name: 'Bob',
+            age: 23,
+            idEnvelope: null
+        },{
+            id: 2,
+            realGender: 'F',
+            name: "Eve",
+            age: 23,
+            idEnvelope: 1
+        }]
+        clinicDependency.getClinic().envelopes = [{
+            id: 1,
+            gender: 'M',
+            age: 23,
+            idStack: 2
+        }]
+
+        const response = clinicDependency.getClinic().assignStackToEnvelope(1)
+        const error404 = {status: 404, message: "No backup envelopes available"}
+        expect(response).toEqual(error404)
+    })
+
+
+
+})
+
+describe('destroyStack', () => {
 
     it('destroy the stack', () => {
         clinicDependency.getClinic().stacks = [{
